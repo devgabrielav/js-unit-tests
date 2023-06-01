4c475ea53915bb25ce1e9d256cb00eeb53ac301b
devgabrielav <gabrivicentini@icloud.com>
2023-06-01 15:58:20 -0300

    Requisito 3!!

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,12 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (name == undefined || age == undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${name}!\nTenho ${age} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`
+};
 
+console.log(vqv('Gabriela'));
 module.exports = vqv;
